0cd5f609ee8d8818789c69f80850e5157bc94603
GoldiL <goldialbina@gmail.com>
2025-06-16 17:53:01 -0500

    Control layer changes

--- src/main/java/com/javapractise/rest_demo/controller/CloudVendorAPIService.java
+++ src/main/java/com/javapractise/rest_demo/controller/CloudVendorAPIService.java
@@ -3,14 +3,21 @@ package com.javapractise.rest_demo.controller;
 import com.javapractise.rest_demo.model.CloudVendor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
 
+    CloudVendor cloudVendor;
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendor){
         return new CloudVendor("C1","Maggie","Address1","3145165256");
+    }
+
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return  "Cloud Vendor Details Recieved";
     }
 }
